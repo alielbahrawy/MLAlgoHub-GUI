7f98da7afcd66d88de430689eff2a69bedf96429
MaryamHassan10 <marryam.hassan10@gmail.com>
2025-04-25 21:13:50 +0300

    processed data summary

--- Core/GUI/PreprocessingPage.py
+++ Core/GUI/PreprocessingPage.py
@@ -6,6 +6,8 @@ from sklearn.impute import SimpleImputer, KNNImputer
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler, StandardScaler
 from scipy import stats
 import tkinter as tk
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
 
 class SecondPage(ctk.CTk):
     def __init__(self, data):
@@ -20,7 +22,7 @@ class SecondPage(ctk.CTk):
         self.details = {
             "Simple Imputer": "mean, median, most_frequent, constant. Useful for numerical & categorical data.",
             "KNN Imputer": "Fills missing values based on nearest neighbors.",
-            "Label Encoder": "Encodes categorical labels as numbers.",
+            "IterativeImputer": "fills missing values by predicting them using other features in the dataset.",
             "One Hot Encoder": "Converts categorical columns into multiple binary columns.",
             "IQR method": "Detects outliers using Interquartile Range.",
             "Z-score": "Detects outliers using standard deviation from mean.",
@@ -52,6 +54,7 @@ class SecondPage(ctk.CTk):
         step1 = self.tree.insert("", "end", text="Handle Missing Values", open=True)
         self.tree.insert(step1, "end", text="Simple Imputer")
         self.tree.insert(step1, "end", text="KNN Imputer")
+        self.tree.insert(step1, "end", text="IterativeImputer")
 
         step2 = self.tree.insert("", "end", text="Data Encoding", open=True)
         self.tree.insert(step2, "end", text="Label Encoder")
@@ -82,13 +85,41 @@ class SecondPage(ctk.CTk):
         try:
             self.apply_step(selected)
             if selected != "Simple Imputer":
+                missing_values_numeric = self.processed_df.select_dtypes(include=[np.number]).isnull().sum().sum()
+                missing_values_object = self.processed_df.select_dtypes(include=['object']).isnull().sum().sum()
+                
                 self.info.insert("end", "\n\n✅ Operation applied successfully.")
+                    
+                    
         except Exception as e:
             self.info.insert("end", f"\n\n❌ Error: {e}")
+            
+    def show_data_summary(self):
+        num_cols = self.processed_df.select_dtypes(include=[np.number]).columns
+
+        # Show head of numeric data
+        self.info.insert("end", f"\n\n{self.processed_df[num_cols].head(10)}")
+        self.info.insert("end", f"\n\n" + "_"*50)
+        
+        # Count of missing values
+        missing_values_count = self.processed_df[num_cols].isnull().sum().sum()
+        self.info.insert("end", f"\n\n Total Missing Values(Numerics): {missing_values_count}")
+        self.info.insert("end", f"\n\n" + "_"*50)
+        
+        # Data types
+        types = self.processed_df.dtypes
+        self.info.insert("end", f"\n\n Data Types:\n{types}")
+        self.info.insert("end", f"\n\n" + "_"*50)
+        
+        # Duplicated rows
+        duplicate_count = self.processed_df.duplicated().sum()
+        self.info.insert("end", f"\n\n Number of Duplicated Rows: {duplicate_count}")
+        self.info.insert("end", f"\n\n" + "_"*50)
+
+            
 
     def apply_step(self, step_name):
         num_cols = self.processed_df.select_dtypes(include=[np.number]).columns
-
         if step_name == "Simple Imputer":
             def on_confirm():
                 strategy = strategy_var.get()
@@ -113,20 +144,46 @@ class SecondPage(ctk.CTk):
             tk.Button(top, text="Apply", command=on_confirm).pack(pady=10)
 
         elif step_name == "KNN Imputer":
-            imputer = KNNImputer(n_neighbors=3)
-            self.processed_df[num_cols] = imputer.fit_transform(self.processed_df[num_cols])
+            if self.processed_df.select_dtypes(include=[np.number]).isnull().sum().sum() == 0:
+                self.info.insert("end", f"\n\ndata doesn't have a missing value")
+            else:
+        
+                imputer = KNNImputer(n_neighbors=2)
+                self.processed_df[num_cols] = imputer.fit_transform(self.processed_df[num_cols])
+                self.show_data_summary()
+
+                
+                
+        elif step_name == "IterativeImputer":
+            if self.processed_df.select_dtypes(include=[np.number]).isnull().sum().sum()== 0:
+                self.info.insert("end", f"\n\ndata doesn't have a missing value")
+            else:
+        
+                imputer = IterativeImputer()
+                self.processed_df[num_cols] = imputer.fit_transform(self.processed_df[num_cols])
+                self.show_data_summary()
+
+              
+              
+                
+                
 
         elif step_name == "Label Encoder":
             le = LabelEncoder()
             for col in self.processed_df.select_dtypes(include=['object']).columns:
                 self.processed_df[col] = le.fit_transform(self.processed_df[col].astype(str))
+            self.show_data_summary()
+
 
         elif step_name == "One Hot Encoder":
             self.processed_df = pd.get_dummies(self.processed_df)
+            self.show_data_summary()
+
 
         elif step_name == "Z-score":
             z_scores = np.abs(stats.zscore(self.processed_df[num_cols]))
             self.processed_df = self.processed_df[(z_scores < 3).all(axis=1)]
+            self.show_data_summary()
 
         elif step_name == "IQR method":
             Q1 = self.processed_df[num_cols].quantile(0.25)
@@ -134,24 +191,34 @@ class SecondPage(ctk.CTk):
             IQR = Q3 - Q1
             mask = ~((self.processed_df[num_cols] < (Q1 - 1.5 * IQR)) | (self.processed_df[num_cols] > (Q3 + 1.5 * IQR))).any(axis=1)
             self.processed_df = self.processed_df[mask]
+            self.show_data_summary()
 
         elif step_name == "Remove Outliers":
             z_scores = np.abs(stats.zscore(self.processed_df[num_cols]))
             self.processed_df = self.processed_df[(z_scores < 3).all(axis=1)]
-
+            self.show_data_summary()
+ 
         elif step_name == "Handle Duplicated":
             self.processed_df = self.processed_df.drop_duplicates()
+            self.show_data_summary()
 
+           
         elif step_name == "Min-Max Normalization":
             scaler = MinMaxScaler()
             self.processed_df[num_cols] = scaler.fit_transform(self.processed_df[num_cols])
-
+            self.show_data_summary()
+ 
         elif step_name == "Standard Scaler":
             scaler = StandardScaler()
             self.processed_df[num_cols] = scaler.fit_transform(self.processed_df[num_cols])
+            self.show_data_summary()
+
 
         elif step_name == "Log Transform":
             self.processed_df = self.processed_df.apply(lambda x: np.log1p(x) if np.issubdtype(x.dtype, np.number) else x)
+            self.show_data_summary()
 
+            
         if step_name != "Simple Imputer":
             self.processed_df.to_csv("processed_data.csv", index=False)
+            
