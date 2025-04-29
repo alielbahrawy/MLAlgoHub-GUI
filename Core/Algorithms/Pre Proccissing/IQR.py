bc83b1648f6487f81e7b26673a86809cb20c3421
Mohmed Alaa <Mohamed.Aalaa5@ai.menofia.edu.eg>
2025-04-29 21:35:22 +0300

    add iqr

--- Core/Algorithms/Pre Proccissing/IQR.py
+++ Core/Algorithms/Pre Proccissing/IQR.py
@@ -1,47 +1,30 @@
 import pandas as pd
 import numpy as np
 
-def handle_outliers_iqr(df):
-    """
-    معالجة القيم الشاذة (Outliers) باستخدام طريقة IQR.
-    يتم استبدال القيم الشاذة بالـ Median لكل عمود رقمي.
-    تُرجع DataFrame جديدة وجدول بعدد القيم الشاذة قبل وبعد المعالجة.
-    """
-
-    df_processed = df.copy()
-    numeric_cols = df_processed.select_dtypes(include=[np.number]).columns
-    outlier_summary = pd.DataFrame(columns=["Before", "After"])
+def detect_outliers_iqr(df):
+  
+    
+    numeric_cols = df.select_dtypes(include=[np.number]).columns
+    outlier_summary = {}
 
     for col in numeric_cols:
-        Q1 = df_processed[col].quantile(0.25)
-        Q3 = df_processed[col].quantile(0.75)
+        Q1 = df[col].quantile(0.25)
+        Q3 = df[col].quantile(0.75)
         IQR = Q3 - Q1
-
         lower_bound = Q1 - 1.5 * IQR
         upper_bound = Q3 + 1.5 * IQR
-
-        # تحديد القيم الشاذة
-        outlier_indices = df_processed[col].index[
-            (df_processed[col] < lower_bound) | (df_processed[col] > upper_bound)
+        outlier_indices = df[col].index[
+            (df[col] < lower_bound) | (df[col] > upper_bound)
         ]
-        count_before = len(outlier_indices)
+        count_outliers = len(outlier_indices)
 
-        # استبدالها بالـ median
-        median_val = df_processed[col].median()
-        df_processed.loc[outlier_indices, col] = median_val
 
-        # إعادة الفحص بعد المعالجة
-        count_after = len(df_processed[
-            (df_processed[col] < lower_bound) | (df_processed[col] > upper_bound)
-        ])
+        outlier_summary[col] = {
+            'count': count_outliers,
+            'indices': outlier_indices.tolist()
+        }
 
-        outlier_summary.loc[col] = [count_before, count_after]
 
-    print("\n✅ عدد القيم الشاذة قبل وبعد المعالجة بطريقة IQR:")
-    print(outlier_summary)
+    return  outlier_summary
 
-    return df_processed, outlier_summary
 
-from data_loader import load_dataset
-df = load_dataset(r"C:\Users\TUF\Documents\My Data Sets\patients.xlsx")
-df_cleaned, summary = handle_outliers_iqr(df)
